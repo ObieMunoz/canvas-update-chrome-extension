a66b418a059870932d8eee6279e4c38c0966e79c
Obie <obiemunozjr@gmail.com>
2021-11-09 16:26:03 -0500

    Added failure checking for username and displays Unknown if incorrectly grabbed

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function updateToDoList() {
     let incompleteTasks = document.querySelectorAll(".icon-mark-as-read");
     let scriptVariables = document.getElementsByTagName("script")[3].text;
     let username = scriptVariables.slice(scriptVariables.indexOf("display_name") + 15, scriptVariables.indexOf("avatar_image") - 3);
+    if (username === "" || username === undefined || username.length > 30) {
+        username = "Unknown";
+    }
     let count = 0;
     let toDoList = document.querySelector(".todo-list")
     if (incompleteTasks.length > 0) {
