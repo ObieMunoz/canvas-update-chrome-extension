525fc2a01f6d1d38bf22a96acabc300a0e0ddb09
Obie <obiemunozjr@gmail.com>
2021-11-09 16:14:11 -0500

    New load detection, cleaned up code

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-(function () {
-    const timer = setTimeout(() => {
-        try {
-            const incompleteTasks = document.querySelectorAll(".icon-mark-as-read")
-            const listTaskNames = [];
-            const scriptVariables = document.getElementsByTagName("script")[3].text
-            const username = scriptVariables.slice(scriptVariables.indexOf("display_name") + 15, scriptVariables.indexOf("avatar_image") - 3)
+let checkCanvasLoaded = setInterval(() => {
+    if (document.readyState === 'complete' && document.querySelectorAll(".icon-mark-as-read").length > 0) {
+        clearInterval(checkCanvasLoaded);
+        init();
+    }
+}, 100);
 
-            let count = 0;
+function updateToDoList() {
+    const listTaskNames = [];
+    let incompleteTasks = document.querySelectorAll(".icon-mark-as-read");
+    let scriptVariables = document.getElementsByTagName("script")[3].text;
+    let username = scriptVariables.slice(scriptVariables.indexOf("display_name") + 15, scriptVariables.indexOf("avatar_image") - 3);
+    let count = 0;
+    let toDoList = document.querySelector(".todo-list")
+    if (incompleteTasks.length > 0) {
+        incompleteTasks.forEach(task => {
+            listTaskNames.push(`${++count}. <a href=${task.parentNode.parentNode.querySelector("a").href}>${task.parentNode.parentNode.querySelector("a").innerText}</a>`)
+        })
+    } else {
+        listTaskNames.push(`You have no incomplete tasks!`)
+    }
+    const newListContainer = document.createElement("div");
+    newListContainer.style.backgroundColor = 'white';
+    newListContainer.style.border = '1px solid red';
+    newListContainer.innerHTML = `<strong>${username}'s REAL To Do List</strong><br><hr>` + listTaskNames.join("<br>") + `<br><br><em>Thanks for using Obie's Canvas Upgrade!</em>`
+    toDoList.replaceChildren(newListContainer)
+}
 
-            if (incompleteTasks.length > 0) {
-                incompleteTasks.forEach(task => {
-                    listTaskNames.push(`${++count}. <a href=${task.parentNode.parentNode.querySelector("a").href}>${task.parentNode.parentNode.querySelector("a").innerText}</a>`)
-                })
-            } else {
-                listTaskNames.push(`WARNING: You either have no incomplete tasks or we had a loading error.`)
-            }
+function checksToStars() {
+    const greenChecks = document.querySelectorAll('.icon-check')
+    greenChecks.forEach(check => {
+        check.className = "icon-star"
+        check.style.color = 'gold'
+    })
+}
 
-            const div = document.createElement("div");
-            const toDoList = document.querySelector(".todo-list")
-            div.style.backgroundColor = 'white';
-            div.style.border = '1px solid red';
-            div.innerHTML = `<strong>${username}'s REAL To Do List</strong><br><hr>` + listTaskNames.join("<br>") + `<br><br><em>Thanks for using Obie's Canvas Upgrade!</em>`
-            toDoList.replaceChildren(div)
-
-        } catch { }
-
-        try {
-            const greenChecks = document.querySelectorAll('.icon-check')
-            greenChecks.forEach(check => {
-                check.className = "icon-star"
-                check.style.color = 'gold'
-            })
-        } catch { }
-
-    }, 2000)
-})();
+function init() {
+    updateToDoList();
+    checksToStars();
+}
